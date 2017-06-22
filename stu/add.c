25145de64c64e23b9395f3b14604f46b83bceb3b
chenmuer <muerchen@outlook.com>
2017-06-21 19:09:11 -0700

    new 	修改：     stu/Makefile 	修改：     stu/add.c 	修改：     stu/add.cgi 	修改：     stu/del.c 	修改：     stu/del.cgi 	修改：     stu/index.html 	修改：     stu/sel.c 	修改：     stu/sel.cgi 	mysql-doc/ 	stu/add_c.c     	stu/del_c.c 	stu/del_cc.c 	stu/footer.html 	stu/head.html 	stu/index_s.html 	stu/index_t.html 	stu/mod_c.c 	stu/sel_c.c 	stu/sel_s.c 	stu/student.c 	stu/teacher.c

--- stu/add.c
+++ stu/add.c
@@ -4,8 +4,12 @@
 #include <mysql/mysql.h>
 #include "cgic.h"
 
+char * headname = "head.html";
+char * footname = "footer.html";
+
 int cgiMain()
 {
+	FILE * fd;
 
 	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
 
@@ -15,6 +19,19 @@ int cgiMain()
 	char sex[16] = "\0";
 	char class[32] = "\0";
 	int status = 0;
+	char ch;
+
+	if(!(fd = fopen(headname, "r"))){
+		fprintf(cgiOut, "Cannot open file, %s\n", headname);
+		return -1;
+	}
+	ch = fgetc(fd);
+
+	while(ch != EOF){
+		fprintf(cgiOut, "%c", ch);
+		ch = fgetc(fd);
+	}
+	fclose(fd);
 
 	status = cgiFormString("sname",  sname, 32);
 	if (status != cgiFormSuccess)
@@ -74,7 +91,7 @@ int cgiMain()
 	}
 
 
-	strcpy(sql, "create table information(sno int(4) not null primary key auto_increment, sname char(20) not null, sex char(10) not null default 'M', sage int(4) not null,class int(4) not null)");
+	strcpy(sql, "create table information(sno int(4) not null primary key auto_increment, sname char(20) not null, sex char(10) not null default 'M', sage int(4) not null,class int(4) not null,val int(4) not null default '2')");
 	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
 	{
 		if (ret != 1)
@@ -87,7 +104,7 @@ int cgiMain()
 
 
 
-	sprintf(sql, "insert into information values(%d, '%s','%s', %d, %d)", atoi(sno), sname,sex, atoi(sage),atoi(class));
+	sprintf(sql, "insert into information values(%d, '%s','%s', %d, %d, '2')", atoi(sno), sname,sex, atoi(sage),atoi(class));
 	if (mysql_real_query(db, sql, strlen(sql) + 1) != 0)
 	{
 		fprintf(cgiOut, "%s\n", mysql_error(db));
